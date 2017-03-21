bf1348d9764d023e308976fca5e8e113339c87f2
Joey Schluchter <jschluchter@gmail.com>
2017-03-21 11:55:25 -0400

    update branch

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Lennar', message: 'A sample website using nodejs with github / vsts deployments' });
 });
 
-router.get('/one', function(req, res, next) {
+router.get('/1', function(req, res, next) {
   res.render('index', { title: 'PAGE 1', message: 'The first page' });
 });
 
-router.get('/two', function(req, res, next) {
+router.get('/2', function(req, res, next) {
   res.render('index', { title: 'PAGE 2', message: 'The second page' });
 });
 
-router.get('/three', function(req, res, next) {
+router.get('/3', function(req, res, next) {
   res.render('index', { title: 'PAGE three', message: 'The third page' });
 });
 
-router.get('/four', function(req, res, next) {
+router.get('/4', function(req, res, next) {
   res.render('index', { title: 'PAGE four', message: 'The third page' });
 });
 
