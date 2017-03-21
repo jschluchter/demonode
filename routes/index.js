01c69955d6db748b93a4f39a0883400df34a5cf6
Joey Schluchter <jschluchter@gmail.com>
2017-03-20 23:56:59 -0400

    updated pages

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Lennar', message: 'A sample website using nodejs with github / vsts deployments' });
 });
 
+router.get('/one', function(req, res, next) {
+  res.render('index', { title: 'PAGE 1', message: 'The first page' });
+});
+
+router.get('/two', function(req, res, next) {
+  res.render('index', { title: 'PAGE 2', message: 'The second page' });
+});
+
 module.exports = router;
